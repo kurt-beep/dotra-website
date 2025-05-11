1e8fcaee0df924e4c3681e7dad84c0148501e6ea
kerim <kerim.kurt96@gmail.com>
2025-05-11 19:41:38 +0200

    admob

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.post('/api/send-email', async (req, res) => {
 });
 
 // Statische Dateien
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Hauptroute
 app.get('/', (req, res) => {
